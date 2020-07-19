7c291bdf93476ee122ef3180ff41a2161b5cce4a
CoCopark777 <825510546@qq.com>
2020-07-19 16:59:31 +0800

    web实验脚本上库

--- test_001.py
+++ test_001.py
@@ -1,33 +1,71 @@
 # coding=utf-8
-
+import time
+import logging
+from selenium import webdriver
 from kuangjia import Test
 
 
-class Test_001(Test):
+class Test001(Test):
 
     def pre_condition_func(self):
-        print("Test_001的前置步骤")
-        pass
+        """
+        用例前置
+
+        """
+        self.driver = webdriver.Chrome()
+        self.driver.get('http://www.weather.com.cn/')
+        print("登录中国天气网成功")
+        time.sleep(2)
+        self.driver.quit()
+        print("退出中国天气网")
 
     def test_procedure(self):
-        print("Test_001具体的测试步骤")
-        pass
+        """
+        具体步骤
+
+        """
+        try:
+            self.driver = webdriver.Chrome()
+            self.driver.get('http://www.weather.com.cn/')
+            print("再次登录中国天气网成功，准备进入登录界面")
+            time.sleep(2)
+            # 点击登录
+            self.driver.find_element_by_class_name("login-icon").click()
+            print("点击登录成功")
+            time.sleep(2)
+            self.driver.find_element_by_id("username").send_keys("825510546@qq.com")
+            print("输入账号")
+            time.sleep(1)
+            self.driver.find_element_by_id("password").send_keys("test0925")
+            print("输入密码")
+            time.sleep(1)
+            self.driver.find_element_by_id("loginBtnId").click()
+            print("点击登录按钮")
+            time.sleep(2)
+            print(self.driver.current_url)
+            assert 'http://www.weather.com.c/' == self.driver.current_url
+            print('登陆成功.')
+            time.sleep(2)
+        except Exception as e:
+            print("登录失败", e)
 
     def post_condition_func(self):
-        print("Test_001环境恢复")
-        pass
+        """
+        环境恢复
 
+        """
+        self.driver.quit()
 
-class Test_002(Test):
-
-    def pre_condition_func(self):
-        print("Test_002的前置步骤")
-        pass
-
-    def test_procedure(self):
-        print("Test_002具体的测试步骤")
-        pass
-
-    def post_condition_func(self):
-        print("Test_002环境恢复")
-        pass
+# class Test_002(Test):
+#
+#     def pre_condition_func(self):
+#         print("Test_002的前置步骤")
+#         pass
+#
+#     def test_procedure(self):
+#         print("Test_002具体的测试步骤")
+#         pass
+#
+#     def post_condition_func(self):
+#         print("Test_002环境恢复")
+#         pass
---
+++ webATE.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+from selenium import webdriver
+import time
+from kuangjia import Test
+import logging
+from logging import handlers
+
+logging.basicConfig(level=logging.WARNING,  # 控制台打印的日志级别
+                    filename="D:\\loginfo\\time.log",
+                    filemode='a',  ##模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
+                    # a是追加模式，默认如果不写的话，就是追加模式
+                    format=
+                    '%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'
+                    # 日志格式
+                    )
+
+
+class Test002(Test):
+
+    def pre_condition_func(self):
+        logging.info("脚本开始，前置步骤")
+
+    def test_guocheng(self):
+        driver = webdriver.Chrome()
+        driver.get('http://www.weather.com.cn/')
+        time.sleep(5)
+        driver.quit()
+        logging.info("打印信息加入，用于生成log文件")
+
+    def post_condition_func(self):
+        logging.info("用例结束")
